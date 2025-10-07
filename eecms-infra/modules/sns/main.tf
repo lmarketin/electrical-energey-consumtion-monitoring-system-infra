d3bc72cf3273379449e3e30c69cf2ef5e1a7a639
lovremarketin <lovremrketin@gmail.com>
2025-10-07 21:46:55 +0200

    email address update

--- eecms-infra/modules/sns/main.tf
+++ eecms-infra/modules/sns/main.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "admin_email_topic" {
 resource "aws_sns_topic_subscription" "admin_email_subscription" {
   topic_arn = aws_sns_topic.admin_email_topic.arn
   protocol  = "email"
-  endpoint  = "lmarketin@tvz.hr"
+  endpoint  = "lovre.marketin@gmail.com"
 }
